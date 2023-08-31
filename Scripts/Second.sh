e820a0d15ca7f8fa9281595fd4428dc6f1090248
chrisbg4m <chris.briggs@gear4music.com>
2023-08-31 08:59:55 +0100

    scripts

---
+++ Scripts/Second.sh
@@ -0,0 +1,6 @@
+cd /home/$USER/Desktop
+mkdir -p --verbose 'Bash Play'
+cd 'Bash Play'
+mkdir -p --verbose Chris/{1..12}
+cd Chris
+ls -l --author
---
+++ Scripts/System Setup.sh
@@ -0,0 +1,9 @@
+#System Setup
+
+FIRST STEP
+
+mkdir Development && cd Development
+
+sudo apt-get install git
+
+Do you want to continue? [Y/n] Y
